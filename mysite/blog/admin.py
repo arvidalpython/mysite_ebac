72d52556224d69718a9c8b22ce7a82d0019b2001
Alexandre Vidal <161221123+arvidalpython@users.noreply.github.com>
2025-10-26 13:29:10 -0300

    feat: add Django admin configurations and model migrations

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# @admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','slug', 'status', 'created_on')
+    list_filter = ('status','created_on')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, PostAdmin)
