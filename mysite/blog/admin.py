43eb6788ed9b88b779191d8e587f38914d508081
Alexandre Vidal <161221123+arvidalpython@users.noreply.github.com>
2025-10-28 22:52:20 -0300

    feat: add Post model with slug, status and created_on fields

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
+
+# Register your models here.
+
 from .models import Post
-
-# @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title','slug', 'status', 'created_on')
-    list_filter = ('status','created_on')
-    search_fields = ('title', 'body')
-    prepopulated_fields = {'slug': ('title',)}
-
-admin.site.register(Post, PostAdmin)
